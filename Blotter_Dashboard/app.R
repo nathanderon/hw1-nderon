0dd5904bf1b6a37aac19b883899d85653a729875
nderon <nderon@andrew.cmu.edu>
2019-09-05 22:13:39 -0400

    Changed library loading for online posting

--- Blotter_Dashboard/app.R
+++ Blotter_Dashboard/app.R
@@ -7,16 +7,14 @@
 #    http://shiny.rstudio.com/
 #
 
-#Load packages, adapted from https://gist.github.com/stevenworthington/3178163
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-packages <- c("ggplot2", "dplyr", "shiny", "DT", "tools", "shinyjs", "shinythemes")
-ipak(packages)
+#Load packages
+library("ggplot2")
+library("dplyr")
+library("shiny")
+library("DT")
+library("tools")
+library("shinyjs")
+library("shinythemes")
 
 #Load data
 blotter_data <- read.csv("Blotter_Data_Trim.csv", header = T)
